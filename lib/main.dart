ffb9f3735b4e11c1478438e7963e3bc60810859b
Arthur Denner <arthurdenner7@gmail.com>
2020-04-13 15:56:22 +0200

    feat: init project

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:quarentena_tech_mobile/src/pages/home_page.dart';
+
+void main() {
+  runApp(QuarentenaTech());
+}
+
+class QuarentenaTech extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Quarentena Tech',
+      theme: ThemeData(
+        fontFamily: 'Press Start 2P',
+        appBarTheme: AppBarTheme(
+          textTheme: TextTheme(
+            headline6: TextStyle(
+              fontFamily: 'Press Start 2P',
+              color: Color(0xFFf25a70),
+            ),
+          ),
+        ),
+      ),
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: Image.asset('assets/img/jedi.png'),
+        title: Text('#Quarentena.Tech'),
+      ),
+      body: SafeArea(
+        child: Container(),
+      ),
+    );
+  }
+}
