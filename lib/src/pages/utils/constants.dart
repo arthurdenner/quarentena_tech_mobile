d18879e1648a7d747e45b2068981054d01d20c33
Arthur Denner <arthurdenner7@gmail.com>
2020-04-13 22:00:42 +0200

    feat: display all attributes from thing

--- lib/src/pages/utils/constants.dart
+++ lib/src/pages/utils/constants.dart
@@ -6,6 +6,8 @@ class AppColors {
   static const background = Color(0xFFeae6da);
   static const emphasis = Color(0xFF7c5e99);
   static const main = Color(0xFFf25a70);
+  static const interactive = Color(0xFFf7d51d);
+  static const highlight = Color(0xFFe76e55);
 }
 
 Map<String, Color> colorMap = {
@@ -13,5 +15,5 @@ Map<String, Color> colorMap = {
   'comunidade': Color(0xFF209cee),
   'cursos': Color(0xFF92cc41),
   'twitch': Color(0xFF9147ff),
-  'youtube': Color(0xFFe76e55),
+  'youtube': AppColors.highlight,
 };
--- lib/src/pages/widgets/thing_card.dart
+++ lib/src/pages/widgets/thing_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:quarentena_tech_mobile/src/models/thing.dart';
+import 'package:quarentena_tech_mobile/src/pages/utils/constants.dart';
 import 'package:quarentena_tech_mobile/src/pages/utils/helpers.dart';
 
 class ThingCard extends StatelessWidget {
@@ -16,42 +17,116 @@ class ThingCard extends StatelessWidget {
     return Card(
       child: Padding(
         padding: const EdgeInsets.all(20),
-        child: Row(
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            _buildImage(thing),
-            SizedBox(width: 20),
-            Flexible(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  Text(thing.title),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: thing.categories.map((category) {
-                      return Container(
-                        color: getColorByCategory(category),
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 10,
-                          vertical: 5,
-                        ),
-                        child: Text(
-                          category,
-                          style: TextStyle(
-                            fontSize: 12,
-                          ),
-                        ),
-                      );
-                    }).toList(),
-                  ),
-                ],
-              ),
+            _buildHeader(),
+            Divider(
+              color: Colors.black38,
+              height: 30,
             ),
+            _buildBody(thing),
+            Divider(
+              color: Colors.black38,
+              height: 30,
+            ),
+            _buildFooter(),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildHeader() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        _buildImage(thing),
+        SizedBox(width: 20),
+        Flexible(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Text(thing.title),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: thing.categories.map((category) {
+                  return Container(
+                    color: getColorByCategory(category),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 10,
+                      vertical: 5,
+                    ),
+                    child: Text(
+                      category,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildBody(Thing thing) {
+    return Column(
+      children: <Widget>[
+        Text(
+          thing.content,
+          style: TextStyle(
+            height: 1.2,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildFooter() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        RaisedButton(
+          color: AppColors.interactive,
+          onPressed: () {},
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Compartilhar',
+                style: TextStyle(
+                  fontSize: 12,
+                ),
+              ),
+              SizedBox(width: 5),
+              Image.asset(
+                'assets/img/heart.png',
+                height: 20,
+              ),
+            ],
+          ),
+        ),
+        SizedBox(width: 20),
+        RaisedButton(
+          color: AppColors.highlight,
+          onPressed: () {},
+          child: Center(
+            child: Text(
+              'Detalhes',
+              style: TextStyle(
+                fontSize: 12,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 
